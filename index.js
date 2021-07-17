bc281b07e671186dc9fac0c39a28038cb9f9525c
Pradip Sarkar <85306445+sarkarpradip2002@users.noreply.github.com>
2021-07-17 12:02:08 +0530

    Add files via upload
    Added some features

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class Book{
         this.Type=Type;
     }
 }
-
+ var number=1;
 function AddBook(){
      inputname=document.getElementById('inputEmail3').value;
      inputauthor=document.getElementById('inputPassword3').value;
@@ -30,17 +30,33 @@ function AddBook(){
     }
     
      AddTable=document.getElementById('tablebody');
-     
-     let BookTotal=new Book(inputname,inputauthor);
-    let Add=` 
-    <tr>
-      <th scope="row"></th>
-      <td>${BookTotal.name}</td>
-      <td>${BookTotal.Author}</td>
-      <td>${Type}</td>
-    </tr>`
 
-    AddTable.innerHTML+=Add;
-     
+     if(inputname=='' || inputauthor=='')
+     {
+       alert("You need to fill all the places")
+     }
+     else{
+     let BookTotal=new Book(inputname,inputauthor);
+     let Add=` 
+     <tr>
+       <td>${number}</td>
+       <td>${BookTotal.name}</td>
+       <td>${BookTotal.Author}</td>
+       <td>${Type}</td>
+     </tr>`
+      AddTable.innerHTML+=Add;
+
+     let navbar=document.querySelector('.navbar');
+     navbar.nextElementSibling.innerHTML=`<div class="alert alert-warning alert-dismissible fade show" role="alert" style="font-size:15px;">
+      Your book  is successfully added in your library!
+     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>`
+     setTimeout(()=>{
+      navbar.nextElementSibling.innerHTML=`<h2 class="heading" style="text-align: center; text-decoration: underline; color: green; margin-top: 10px; " >Book Details</h2>`
+      },4000);
+
+     number++;
 }
+}
+
 
